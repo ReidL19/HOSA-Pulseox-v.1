e95a1d5db415cab1eaf1309806b21c67232c9e97
ReidL19 <153392180+ReidL19@users.noreply.github.com>
2023-12-09 12:12:31 -0800

    Update blue_LTE.ino
    -Added Activation sound effect on Activation
    -Blue LED blinks for 5 seconds instead of 10 (reduces battery power used over the course of a period of time.

--- blue_LTE.ino
+++ blue_LTE.ino
@@ -1,13 +1,36 @@
-const int ledPin = 13;  // Connect an LED to pin 13
+// Pin configurations
+const int buzzerPin = 8; // Connect the piezo buzzer to pin 8
+const int ledPin = 13;   // Connect the LED to pin 13
 
 void setup() {
-  pinMode(ledPin, OUTPUT);  // Initialize the LED pin as an output
+  // Set up the buzzer and LED pins
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+
+  // Play activation sound
+  playActivationSound();
+
+  // Blink the LED for 5 seconds
+  blinkLED();
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH); // Turn on the LED
-  delay(500);                // Wait for 1 second (1000 milliseconds)
-  digitalWrite(ledPin, LOW);  // Turn off the LED
-  delay(500);                // Wait for 1 second
+  // Nothing to do here
+}
 
+void playActivationSound() {
+  // Play a simple tone for the activation sound
+  tone(buzzerPin, 1000, 500); // 1000 Hz for 500 milliseconds
+  delay(1000); // Pause for 1 second after the sound
+  noTone(buzzerPin); // Stop the tone
+}
+
+void blinkLED() {
+  // Blink the LED for 5 seconds
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(ledPin, HIGH); // Turn the LED on
+    delay(500); // Wait for 500 milliseconds
+    digitalWrite(ledPin, LOW); // Turn the LED off
+    delay(500); // Wait for 500 milliseconds
+  }
 }
